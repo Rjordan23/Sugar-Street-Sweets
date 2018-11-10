85a71d9c8ad9a1eb8fc7cd68cfdbcb5bbfaaf26b
Ramsey Jordan <ramsey.jordan.84@gmail.com>
2018-11-10 03:19:48 -0500

    added images to placeholder tiles, started the cart and added new color

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,36 @@
+// Show Cart
 document.getElementById('cart-info').addEventListener('click', function() {
   const cart = document.getElementById('cart');
   cart.classList.toggle('show-cart');
   console.log(cart);
 });
+
+// add items to the cart
+
+(function(){
+
+  const cartBtn = document.querySelectorAll('.store-item-icon');
+
+  cartBtn.forEach(function(btn) {
+    btn.addEventListener('click', function(event) {
+    // console.log(event.target);
+
+    if(event.target.parentElement.classList.contains('store-item-icon')) {
+
+      let fullPath = event.target.parentElement.previousElementSibling.src;
+
+      let pos = fullPath.indexOf('img') + 3;
+
+      let partPath = fullPath.slice(pos);
+      
+
+
+      console.log(partPath)
+    }
+
+    })
+  })
+
+
+
+})();
