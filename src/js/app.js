bfd1237d7494848ba97e9b5c7393d3bc11e700d1
Ramsey Jordan <ramsey.jordan.84@gmail.com>
2018-11-10 11:51:52 -0500

    added function to add items to the cart

--- src/js/app.js
+++ src/js/app.js
@@ -18,14 +18,54 @@ document.getElementById('cart-info').addEventListener('click', function() {
     if(event.target.parentElement.classList.contains('store-item-icon')) {
 
       let fullPath = event.target.parentElement.previousElementSibling.src;
-
       let pos = fullPath.indexOf('img') + 3;
-
       let partPath = fullPath.slice(pos);
-      
+
+      const item = {};
+        item.img = `cart-img${partPath}`
+
+      let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+        item.name = name;
+
+      let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+
+      let finalPrice = price.slice(1).trim();
+
+      item.price = finalPrice
 
 
-      console.log(partPath)
+      const cartItem = document.createElement('div');
+        cartItem.classList.add(
+          'cart-item',
+          'd-flex',
+          'justify-content-between',
+          'text-capitalize',
+          'my-3'
+        );
+
+        cartItem.innerHTML = `
+        <!-- https://www.iconfinder.com/icons/61138/cake_donut_icon -->
+        <img src= ${item.img}"img/cart-img/donut.png" class="img-fluid" id="item-img" alt="">
+        <div class="item-text">
+          <p id="cart-item-title" class="cart-item-title font-weight-bold mb-0">${item.name}</p>
+          <span>$</span>
+          <span id="cart-item-price" class="cart-item-price mb-0">${item.price}</span>
+        </div>
+        <a href="#" id="cart-item-remove" class="cart-item-remove">
+            <i class="fas fa-trash-alt"></i>
+        </a>
+      </div>
+      `;
+
+      // Select Cart
+      const cart = document.getElementById('cart');
+      const total = document.querySelector('.cart-total-container');
+
+      cart.insertBefore(cartItem, total);
+      alert('item added to the cart!');
+
+
+
     }
 
     })
